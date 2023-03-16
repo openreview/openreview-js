deea1458a4fd1738a9f51b115f6db7d4e31b2f61
Carlos Mondragon <carlos@openreview.net>
2023-03-16 15:07:04 -0600

    Refactor: Rename params to the corresponding edit type

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ const { OpenReviewClient } = require('../index');
 
 describe('OpenReview Client', function () {
   this.beforeAll(async function () {
-    this.superUser = 'test@openreview.net';
+    this.superUser = 'OpenReview.net';
     this.superUserPassword = '1234';
     this.superClient = new OpenReviewClient('http://localhost:3001');
     await this.superClient.resetPassword(this.superUser, this.superUserPassword);
@@ -12,10 +12,12 @@ describe('OpenReview Client', function () {
   it('should be able to connect to the OpenReview', async function () {
     const data = await this.superClient.connect(this.superUser, this.superUserPassword);
     assert.equal(!!data.token, true);
+    assert.equal(!!data.user, true);
+    assert.equal(data.error, null);
   });
 
   it('should register a new user', async function () {
-    const { user } = await this.superClient.registerUser({
+    const { user, error } = await this.superClient.registerUser({
       email: 'new_user@email.com',
       first: 'New',
       last: 'User',
@@ -23,6 +25,7 @@ describe('OpenReview Client', function () {
     });
     assert.equal(user.id.startsWith('~New_User'), true);
     assert.equal(user.state, 'Inactive');
+    assert.equal(error, null);
   });
 
   it('should search profiles', async function () {
@@ -35,19 +38,140 @@ describe('OpenReview Client', function () {
     assert.equal(user.id.startsWith('~Searchable_User'), true);
     assert.equal(user.state, 'Inactive');
 
-    const { profiles: termProfiles } = await this.superClient.searchProfiles({ term: 'Searchable' });
-    assert.equal(termProfiles.length > 0, true);
+    const termData = await this.superClient.searchProfiles({ term: 'Searchable' });
+    assert.equal(termData.profiles.length === 1, true);
+    assert.equal(termData.count === 1, true);
+    assert.equal(termData.error, null);
 
-    const { profiles: emailProfiles } = await this.superClient.searchProfiles({
+    const emailData = await this.superClient.searchProfiles({
       emails: [ 'searchable_user@email.com' ]
     });
-    assert.equal(emailProfiles.length === 1, true);
+    assert.equal(emailData.profiles.length === 1, true);
+    assert.equal(emailData.count === 1, true);
+    assert.equal(emailData.error, null);
 
-    const { profiles: tildeProfiles } = await this.superClient.searchProfiles({
+    const tildeData = await this.superClient.searchProfiles({
       ids: [ '~Searchable_User1' ]
     });
-    assert.equal(tildeProfiles.length === 1, true);
+    assert.equal(tildeData.profiles.length === 1, true);
+    assert.equal(tildeData.count === 1, true);
+    assert.equal(tildeData.error, null);
   });
 
+  it('should POST and GET an Invitation', async function () {
+    let res = await this.superClient.postInvitationEdit({
+      writers: [ this.superUser ],
+      readers: [ this.superUser ],
+      signatures: [ this.superUser ],
+      invitation: {
+        id: `${this.superUser}/-/Edit`,
+        signatures: [ this.superUser ],
+        writers: [ this.superUser ],
+        invitees: [ this.superUser ],
+        readers: [ this.superUser ],
+        edit: true
+      }
+    });
+    assert.equal(res.error, null);
+    assert.equal(res.edit.invitation.id, `${this.superUser}/-/Edit`);
+    assert.equal(res.edit.invitation.writers[0], this.superUser);
+    assert.equal(res.edit.invitation.readers[0], this.superUser);
+    assert.equal(res.edit.invitation.invitees[0], this.superUser);
+    assert.equal(res.edit.invitation.signatures[0], this.superUser);
+    assert.equal(res.edit.invitation.edit, true);
 
+    res = await this.superClient.getInvitations({
+      id: `${this.superUser}/-/Edit`
+    });
+    assert.equal(res.error, null);
+    assert.equal(res.invitations.length, 1);
+    assert.equal(res.invitations[0].id, `${this.superUser}/-/Edit`);
+    assert.equal(res.count, 1);
+  });
+
+  it('should POST and GET a Group', async function () {
+    let res = await this.superClient.postInvitationEdit({
+      writers: [ this.superUser ],
+      readers: [ this.superUser ],
+      signatures: [ this.superUser ],
+      invitation: {
+        id: `${this.superUser}/-/Edit`,
+        signatures: [ this.superUser ],
+        writers: [ this.superUser ],
+        invitees: [ this.superUser ],
+        readers: [ this.superUser ],
+        edit: true
+      }
+    });
+    assert.equal(res.error, null);
+
+    res = await this.superClient.postGroupEdit({
+      invitation: `${this.superUser}/-/Edit`,
+      signatures: [ this.superUser ],
+      writers: [ this.superUser ],
+      readers: [ this.superUser ],
+      group: {
+        id: `${this.superUser}/Test_Group`,
+        signatures: [ this.superUser ],
+        writers: [ this.superUser ],
+        readers: [ this.superUser ],
+        members: [ this.superUser ],
+        signatories: [ this.superUser ]
+      }
+    });
+    assert.equal(res.error, null);
+
+    res = await this.superClient.getGroups({
+      id: `${this.superUser}/Test_Group`
+    });
+    assert.equal(res.error, null);
+    assert.equal(res.groups.length, 1);
+    assert.equal(res.groups[0].id, `${this.superUser}/Test_Group`);
+    assert.equal(res.count, 1);
+  });
+
+  it('should POST and GET a Note', async function () {
+    let res = await this.superClient.postInvitationEdit({
+      writers: [ this.superUser ],
+      readers: [ this.superUser ],
+      signatures: [ this.superUser ],
+      invitation: {
+        id: `${this.superUser}/-/Edit`,
+        signatures: [ this.superUser ],
+        writers: [ this.superUser ],
+        invitees: [ this.superUser ],
+        readers: [ this.superUser ],
+        edit: true
+      }
+    });
+    assert.equal(res.error, null);
+
+    res = await this.superClient.postNoteEdit({
+      writers: [ this.superUser ],
+      readers: [ 'everyone' ],
+      signatures: [ this.superUser ],
+      invitation: `${this.superUser}/-/Edit`,
+      note: {
+        signatures: [ this.superUser ],
+        readers: [ 'everyone' ],
+        writers: [ this.superUser ],
+        content: {
+          title: {
+            value: 'this is a title'
+          }
+        }
+      }
+    });
+    assert.equal(res.error, null);
+    assert.equal(!!res.edit.note.id, true);
+    const noteId = res.edit.note.id;
+
+    res = await this.superClient.getNotes({
+      id: noteId
+    });
+    assert.equal(res.error, null);
+    assert.equal(res.notes.length, 1);
+    assert.equal(res.notes[0].id, noteId);
+    assert.equal(res.count, 1);
+  });
 });
