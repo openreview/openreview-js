797f2ca42cd43fa9f514de0920b779313eefc0db
xkopenreview <xukun@openreview.net>
2026-09-10 16:50:53 -0400

    Refactor: Update profile retrieval tests for clarity and error handling

--- packages/client/test/test.js
+++ packages/client/test/test.js
@@ -541,8 +541,14 @@ describe('OpenReview Client', function () {
 
   });
 
-  it('should GET a profile with no params', async function () {
+  it('should not GET a profile with no params', async function () {
     let res = await this.superClient.getProfiles();
+    assert.equal(res.error.message, 'Profile query parameters are required');
+    assert.equal(res.profiles[0].id, '~Super_User1');
+  });
+
+  it('should GET a profile with id', async function () {
+    let res = await this.superClient.getProfiles({ id: '~Super_User1' });
     assert.equal(res.error, null);
     assert.equal(res.profiles[0].id, '~Super_User1');
   });
